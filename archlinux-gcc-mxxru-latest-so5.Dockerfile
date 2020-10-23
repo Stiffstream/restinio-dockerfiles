ca0b50cd4c8dd169ec2d66720faf4470cc3ad93f
Yauheni Akhotnikau <eao197@gmail.com>
2020-10-23 09:13:51 +0300

    New image added.

---
+++ archlinux-gcc-mxxru-latest-so5.Dockerfile
@@ -0,0 +1,32 @@
+FROM archlinux/base:latest
+
+# Prepare build environment
+RUN pacman -Sy --noconfirm gcc \
+	&& pacman -Sy --noconfirm ruby rubygems rake \
+	&& pacman -Sy --noconfirm wget \
+	&& pacman -Sy --noconfirm git \
+	&& pacman -Sy --noconfirm openssl
+
+RUN pacman -Sy --noconfirm tar gzip unzip
+
+RUN pacman -Sy --noconfirm boost
+
+RUN gem install Mxx_ru
+
+ARG hgrev=HEAD
+
+RUN echo "*** Downloading RESTinio ***" \
+	&& cd /tmp \
+	&& git clone https://github.com/stiffstream/restinio \
+	&& cd restinio \
+	&& git checkout $hgrev
+
+RUN echo "*** Extracting RESTinio's Dependencies ***" \
+	&& export PATH=${PATH}:~/.gem/ruby/2.7.0/bin \
+	&& cd /tmp/restinio \
+	&& RESTINIO_USE_LATEST_SO5=ON mxxruexternals
+
+RUN echo "*** Building RESTinio ***" \
+	&& cd /tmp/restinio/dev \
+	&& MXX_RU_CPP_TOOLSET=gcc_linux RESTINIO_USE_LATEST_SO5=ON ruby build.rb --mxx-cpp-release
+
--- build_all.rb
+++ build_all.rb
@@ -4,11 +4,11 @@ require 'optparse'
 require 'ostruct'
 
 def parse_options
-	result = OpenStruct.new(:hgrev => 'HEAD')
+  result = OpenStruct.new(:hgrev => 'HEAD', :images => Array.new)
 
-	parser = OptionParser.new
+  parser = OptionParser.new
 
-	parser.banner = <<BANER
+  parser.banner = <<BANER
 Build all Dockerfiles in the current directory.
 
 Usage:
@@ -17,34 +17,44 @@ Usage:
 
 BANER
 
-	parser.on('--hgrev REV', "Revision to use (default: #{result.hgrev})") do |rev|
-		result.hgrev = rev
-	end
+  parser.on('--hgrev REV', "Revision to use (default: #{result.hgrev})") do |rev|
+    result.hgrev = rev
+  end
 
-	parser.on('-h', '--help', 'Show this message') do
-		puts parser
-		exit(1)
-	end
+  parser.on('--image Dockerfile-name', "A Dockerfile to be used for build" ) do |name|
+    result.images << name
+  end
 
-	parser.parse!
+  parser.on('-h', '--help', 'Show this message') do
+    puts parser
+    exit(1)
+  end
 
-	result
+  parser.parse!
+
+  result
 end
 
-def run_docker(hgrev)
-	files = Dir['*.Dockerfile']
-	files.each do |dockerfile|
-		tag = "restinio-#{File.basename(dockerfile, '.Dockerfile')}"
-		log_file = "outputs/#{tag}.#{hgrev}.log"
-		cmdline = "docker build -t #{tag} -f #{dockerfile} " +
-				" --build-arg hgrev=#{hgrev} . | tee #{log_file} 2>&1"
-		r = system(cmdline)
-		if not r
-			raise "build failed!\ncmdline: #{cmdline}\nexit code: #{$?}"
-		end
-	end
+def run_docker(hgrev, files)
+  files.each do |dockerfile|
+    tag = "restinio-#{File.basename(dockerfile, '.Dockerfile')}"
+    log_file = "outputs/#{tag}.#{hgrev}.log"
+    cmdline = "docker build -t #{tag} -f #{dockerfile} " +
+        " --build-arg hgrev=#{hgrev} . | tee #{log_file} 2>&1"
+    r = system(cmdline)
+    if not r
+      raise "build failed!\ncmdline: #{cmdline}\nexit code: #{$?}"
+    end
+  end
 end
 
 opts = parse_options
-run_docker(opts.hgrev)
+
+if opts.images.empty?
+  opts.images = Dir['*.Dockerfile']
+end
+
+puts "Dockerfiles to process:\n#{opts.images.join('\n')}\n==="
+
+run_docker(opts.hgrev, opts.images)
 
